90875dc740f1056dce2b70aadfcc66ffcac71b60
Amirul Mu'minin <muminin71@gmail.com>
2023-02-28 13:03:35 +0700

    04. Row and Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,32 @@
-import 'package:belajar_flutter/03_text.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("04. Row and Column"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("Kolom 1"),
+            Text("Kolom 2"),
+            Text("Kolom 3"),
+            Row(
+              children: <Widget>[
+                Text("Kolom 4"),
+                Text("Kolom 5"),
+                Text("Kolom 6"),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
