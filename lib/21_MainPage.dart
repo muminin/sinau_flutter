7c924fda67e4f0887fe4a8e4e4f75e1bd1407d41
Amirul Mu'minin <muminin71@gmail.com>
2023-03-01 12:06:39 +0700

    21. MediaQuery (Responsive App)

---
+++ lib/21_MainPage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("21. MediaQuery (Responsive App)"),
+      ),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+          ? Column(
+              children: generateContainers(),
+            )
+          : Row(
+              children: generateContainers(),
+            ),
+    );
+  }
+
+  List<Widget> generateContainers() {
+    return <Widget>[
+      Container(
+        color: Colors.red,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.green,
+        width: 100,
+        height: 100,
+      ),
+      Container(
+        color: Colors.blue,
+        width: 100,
+        height: 100,
+      ),
+    ];
+  }
+}
