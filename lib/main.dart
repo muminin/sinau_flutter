8e1c9e25114e6cc39fdddf18ddef3166774ef5a7
Amirul Mu'minin <muminin71@gmail.com>
2023-03-08 11:45:54 +0700

    61. MOBX State Management

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+/// https://www.youtube.com/watch?v=NfcwHRRjfPA&list=PLZQbl9Jhl-VACm40h5t6QMDB92WlopQmV&index=65
+///
+/// dependencies:
+/// build_runner: ^1.7.1
+/// flutter_bloc: ^3.0.0
+/// flutter_mobx: ^1.1.0+2
+/// mobx: ^1.2.1+4
+/// mobx_codegen: ^1.1.2
